b9cb0faf6cdd2fa6394e97d3cae928552f1cefd4
el3ctricSol <aubrey.ky.young@gmail.com>
2018-02-27 02:17:01 -0800

    complete phase 1

--- js/scripts.js
+++ js/scripts.js
@@ -2,62 +2,73 @@
 
 	// SECTION 1 ONLY
 
-function image_click() {
-	$('#sect1 img.board').removeClass('offstage');
-}
+// function image_click() {
+// 	$('#sect1 img.board').removeClass('offstage');
+// }
 
-$('#sect1 img.board').click(image_click);
+// $('#sect1 img.board').click(image_click);
 
 
-	// ALL IMAGES
+// 	// ALL IMAGES
 
-function all_image_click() {
-	$(this).removeClass('offstage');
-}
-$('.inview-block img').click(all_image_click);
+// function all_image_click() {
+// 	$(this).removeClass('offstage');	
+// }
+// $('.inview-block img').click(all_image_click);
 
 
 // GET THE 'IN VIEWPORT' GOING
 
 function sect1_onStage() {
 	if ($('#sect1 .inview-block').is(':in-viewport')) {
-		$('#sect1 img.board').removeClass('offstage');
+		$('#sect1 img.board1').removeClass('offstage');
+		$('#sect1 img.b-group').removeClass('offstage');
+		$('#sect1 img.w-group').removeClass('offstage');
 	}
 	else {
-		$('#sect1 img.board').addClass('offstage');
+		$('#sect1 img.board1').addClass('offstage');
+		$('#sect1 img.b-group').addClass('offstage');
+		$('#sect1 img.w-group').addClass('offstage');
 	}
 }
 
 function sect2_onStage() {
 	if ($('#sect2 .inview-block').is(':in-viewport')) {
-		$('#sect2 img.fifties-1955').removeClass('offstage');
-		$('#sect2 img.fifties-car').removeClass('offstage');
+		$('#sect2 img.board2').removeClass('offstage');
+		$('#sect2 img.w-king').removeClass('offstage');
+		$('#sect2 img.w-pawn').removeClass('offstage');
+
 	}
 	else {
-		$('#sect2 img.fifties-1955').addClass('offstage');
-		$('#sect2 img.fifties-car').addClass('offstage');
-
+		$('#sect2 img.board2').addClass('offstage');
+		$('#sect2 img.w-king').addClass('offstage');
+		$('#sect2 img.w-pawn').addClass('offstage');
 	}
 }
 
 function sect3_onStage() {
 	if ($('#sect3 .inview-block').is(':in-viewport')) {
-		$('#sect3 img.sixties-1962').removeClass('offstage');
-		$('#sect3 img.sixties-car').removeClass('offstage');
+		$('#sect3 img.board3').removeClass('offstage');
+		$('#sect3 img.w-rook').removeClass('offstage');
+		$('#sect3 img.w-bishop').removeClass('offstage');
 	}
 	else {
-		$('#sect3 img.sixties-1962').addClass('offstage');
-		$('#sect3 img.sixties-car').addClass('offstage');
-
+		$('#sect3 img.board3').addClass('offstage');
+		$('#sect3 img.w-rook').addClass('offstage');
+		$('#sect3 img.w-bishop').addClass('offstage');
 	}
 }
 
 function sect4_onStage() {
 	if ($('#sect4 .inview-block').is(':in-viewport')) {
-		$('#sect4 img.sixties-1969').removeClass('offstage');
+		$('#sect4 img.board4').removeClass('offstage');
+		$('#sect4 img.w-queen').removeClass('offstage');
+		$('#sect4 img.w-knight').removeClass('offstage');
 	}
 	else {
-		$('#sect4 img.sixties-1969').addClass('offstage');
+		$('#sect4 img.board4').addClass('offstage');
+		$('#sect4 img.w-queen').addClass('offstage');
+		$('#sect4 img.w-knight').addClass('offstage');
 	}
 }
 
