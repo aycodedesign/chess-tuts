57f2947fa8351fe9a0e35559ab6c4246deec66fd
el3ctricSol <aubrey.ky.young@gmail.com>
2018-03-04 01:40:44 -0800

    cross pawns on shield, update in between comments

--- js/scripts.js
+++ js/scripts.js
@@ -24,12 +24,12 @@ console.log('BANGBANG');
 
 function sect1_onStage() {
 	if ($('#sect1 .inview-block').is(':in-viewport')) {
-		$('#sect1 img.board1').removeClass('offstage');
+		$('#sect1 img.board').removeClass('offstage');
 		$('#sect1 img.b-group').removeClass('offstage');
 		$('#sect1 img.w-group').removeClass('offstage');
 	}
 	else {
-		$('#sect1 img.board1').addClass('offstage');
+		$('#sect1 img.board').addClass('offstage');
 		$('#sect1 img.b-group').addClass('offstage');
 		$('#sect1 img.w-group').addClass('offstage');
 	}
@@ -37,13 +37,13 @@ function sect1_onStage() {
 
 function sect2_onStage() {
 	if ($('#sect2 .inview-block').is(':in-viewport')) {
-		$('#sect2 img.board2').removeClass('offstage');
+		$('#sect2 img.board').removeClass('offstage');
 		$('#sect2 img.w-king').removeClass('offstage');
 		$('#sect2 img.w-pawn').removeClass('offstage');
 
 	}
 	else {
-		$('#sect2 img.board2').addClass('offstage');
+		$('#sect2 img.board').addClass('offstage');
 		$('#sect2 img.w-king').addClass('offstage');
 		$('#sect2 img.w-pawn').addClass('offstage');
 	}
@@ -51,12 +51,12 @@ function sect2_onStage() {
 
 function sect3_onStage() {
 	if ($('#sect3 .inview-block').is(':in-viewport')) {
-		$('#sect3 img.board3').removeClass('offstage');
+		$('#sect3 img.board').removeClass('offstage');
 		$('#sect3 img.w-rook').removeClass('offstage');
 		$('#sect3 img.w-bishop').removeClass('offstage');
 	}
 	else {
-		$('#sect3 img.board3').addClass('offstage');
+		$('#sect3 img.board').addClass('offstage');
 		$('#sect3 img.w-rook').addClass('offstage');
 		$('#sect3 img.w-bishop').addClass('offstage');
 	}
@@ -64,12 +64,12 @@ function sect3_onStage() {
 
 function sect4_onStage() {
 	if ($('#sect4 .inview-block').is(':in-viewport')) {
-		$('#sect4 img.board4').removeClass('offstage');
+		$('#sect4 img.board').removeClass('offstage');
 		$('#sect4 img.w-queen').removeClass('offstage');
 		$('#sect4 img.w-knight').removeClass('offstage');
 	}
 	else {
-		$('#sect4 img.board4').addClass('offstage');
+		$('#sect4 img.board').addClass('offstage');
 		$('#sect4 img.w-queen').addClass('offstage');
 		$('#sect4 img.w-knight').addClass('offstage');
 	}
