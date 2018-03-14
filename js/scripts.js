fc9cea0b9e3176c5e5a9509d3f1b45c9d13ed514
Student <student@2035-01-304-04.local>
2018-03-13 20:06:38 -0700

    add scrolling navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,50 @@
-console.log('BANGBANG');
+// $(document).ready(function () {
+
+	/*-------------------------------------
+    Navbar Scroll
+    -------------------------------------*/
+
+    //whenever a nav anchor is clicked, do what is in the brackets
+    $('a').click(function () {
+
+        //1. kill the default behavior
+        event.preventDefault();
+
+        //2. figure out which section to animate to
+
+        // this variable adjusts distance from the section where scroll stops
+        const offset = 200;
+
+        const whichSection = $(this).attr('href');
+
+        //3. animate to section
+
+        //https://stackoverflow.com/questions/832860/how-to-scroll-the-window-using-jquery-scrollto-function
+        $('html, body').animate({
+            scrollTop: $(whichSection).offset().top - offset
+        }, 1500);
+
+    //determines how far down the page navbar sticks
+    $(window).scroll(function () {
+        const howFar = $(window).scrollTop();
+        console.log(howFar);
+
+        if (howFar >= 595) {
+            $('nav').addClass('pinned navbar-dark'),
+			$('nav').removeClass('bg-light');
+        } else {
+            {
+                $('nav').removeClass('pinned navbar-dark');
+				$('nav').addClass('bg-light');
+            }
+        }
+    });
+}); // END a.click function
 
 
-// TRIGGERING THE ANIMATIONS
-
-	// SECTION 1 ONLY
-
-// function image_click() {
-// 	$('#sect1 img.board').removeClass('offstage');
-// }
-
-// $('#sect1 img.board').click(image_click);
-
-
-// 	// ALL IMAGES
-
-// function all_image_click() {
-// 	$(this).removeClass('offstage');
-// }
-// $('.inview-block img').click(all_image_click);
-
-
-// GET THE 'IN VIEWPORT' GOING
+	/*-------------------------------------
+	Inview Animation
+	-------------------------------------*/
 
 function sect1_onStage() {
 	if ($('#sect1 .inview-block').is(':in-viewport')) {
@@ -88,20 +112,20 @@ $(window).scroll(sect4_onStage);
 
 function parallax_move(theMessage, posLeft, scrollSpeed) {
 
-	var sectHowFar = theMessage.offset().top;
+	const sectHowFar = theMessage.offset().top;
 	// shows how far user has scrolled, per scroll
-	var scrollFromTop = $(window).scrollTop() + 200;
+	const scrollFromTop = $(window).scrollTop() + 300;
 
-	var posTop = (scrollFromTop - sectHowFar) * scrollSpeed;
+	const posTop = (scrollFromTop - sectHowFar) * scrollSpeed;
 
 	// theMessage.css('background-position', '-100px 600px, center center')
 	theMessage.css('background-position', posLeft + ' ' + posTop + 'px, center center');
-	console.log(theMessage);
 }
 
 $(window).scroll(function () {
-	parallax_move($('#message1'), '0', 0.4);
-	parallax_move($('#message2'), '0', 0.2);
+	parallax_move($('#message1'), '0', 0.7);
+	parallax_move($('#message2'), '0', 0.7);
 	parallax_move($('#message3'), '0', 0.7);
-	parallax_move($('#message4'), '0', 0.2);
+	parallax_move($('#message4'), '0', 0.7);
 });
+//end READY FUNCTION
